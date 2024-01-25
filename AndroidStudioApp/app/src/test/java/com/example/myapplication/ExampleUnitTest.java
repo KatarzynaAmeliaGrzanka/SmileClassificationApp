3542de8ab4469f3a8153f861776b3608f983da44
KatarzynaAmeliaGrzanka <katarzyna.a.grzanka@gmail.com>
2024-01-25 18:55:21 +0100

    Name changed

---
+++ AndroidStudioApp/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    //}
+}
---
+++ AndroidStudioApp/sdk/build/intermediates/cxx/Debug/5c6yp5g1/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\pazie\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\pazie\\AndroidStudioProjects\\MyApplication9\\sdk\\.cxx\\Debug\\5c6yp5g1\\arm64-v8a" ^
+  opencv_jni_shared
