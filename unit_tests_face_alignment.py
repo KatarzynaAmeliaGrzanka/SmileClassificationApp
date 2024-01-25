64200a4ba8ba74a702eff3d0d12404b80a6196f8
KatarzynaAmeliaGrzanka <katarzyna.a.grzanka@gmail.com>
2024-01-25 16:34:24 +0100

    unit tests for face detection and face alignment

---
+++ unit_tests_face_alignment.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+from face_normalization import align_face, align_faces_in_dir
+import pathlib as pl
+
+class UnitTestDetectFaces(unittest.TestCase):
+    def test_not_existing_source_dir(self):
+        with self.assertRaises(IOError) and self.assertRaises(SystemExit):
+            align_faces_in_dir("non-existing-directory", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/existing_directory")
+
+    def test_not_existing_destination_dir(self):
+        with self.assertRaises(IOError) and self.assertRaises(SystemExit):
+            align_faces_in_dir("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/existing_directory", "non-existing-directory")
+
+    def test_new_image_in_destination_dir(self):
+        align_faces_in_dir("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/align_source_1", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/align_destination_1")
+        self.assertTrue(os.listdir("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/align_destination_1"))
+
+    def test_no_face_on_image(self):
+         with self.assertRaises(IOError):
+            align_faces_in_dir("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/align_source_2", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/align_destination_2")
+
+    def test_not_image(self):
+        test = align_face("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/align_source_3/not-image.txt", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/align_destination_2/not-image.jpg")
+        self.assertEqual(test,1)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ unit_tests_face_detection.py
@@ -0,0 +1,33 @@
+import unittest
+import os
+from face_detection import detect_faces_in_directory
+import pathlib as pl
+
+class UnitTestDetectFaces(unittest.TestCase):
+    def test_not_existing_source_dir(self):
+        with self.assertRaises(IOError) and self.assertRaises(SystemExit):
+            detect_faces_in_directory("non-existing-directory", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/existing_directory")
+
+    def test_not_existing_destination_dir(self):
+        with self.assertRaises(IOError) and self.assertRaises(SystemExit):
+            detect_faces_in_directory("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/existing_directory", "non-existing-directory")
+
+    def test_new_image_in_destination_dir(self):
+        detect_faces_in_directory("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/source2", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/destination2")
+        self.assertTrue(os.listdir("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/destination2"))
+
+    def test_image_path_exists(self):
+        with self.assertRaises(IOError):
+            detect_faces_in_directory("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/source", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/destination")
+
+    def test_not_image_in_source_file(self):
+        with self.assertRaises(IOError):
+            detect_faces_in_directory("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/source3", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/destination3")
+
+    def test_faces_count(self):
+        faces_count = detect_faces_in_directory("C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/source4", "C:/Users/pazie/Documents/Computer Science/SmileClassificationApp_data/unit-tests-data/destination4")
+        expected_faces_count = 5
+        self.assertEqual(faces_count, expected_faces_count)
+
+if __name__ == '__main__':
+    unittest.main()
